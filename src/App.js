f42d0e86f428d4225098196f6cdccfc6bd3f7ef6
CTGitWhiz <cem276@outlook.de>
2023-02-14 15:35:40 +0100

    Add List Componennt & handleAddActivity function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
-import Form from './components/form';
+import "./App.css";
+import List from "./components/list";
+import Form from "./components/form";
+import useLocalStorageState from "use-local-storage-state";
+import { uid } from "uid";
 
 function App() {
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
+
+  console.log(activities);
+  function handleAddActivity(data) {
+    setActivities([
+      { id: uid(), name: data.name, isForGoodWeather: data.isForGoodWeather },
+      ...activities,
+    ]);
+    console.log(activities);
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
+      <List activities={activities} />
     </div>
   );
 }
